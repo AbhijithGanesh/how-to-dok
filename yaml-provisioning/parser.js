6690771e2e82ffd71e572af4f20ec8ddb7f89b17
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2022-01-27 20:36:34 +0530

    Addition of YAML format and parser

---
+++ yaml-provisioning/parser.js
@@ -0,0 +1,10 @@
+const yaml = require("js-yaml");
+const fs = require("fs");
+
+try{
+    let doc = yaml.load(fs.readFileSync("./format.yaml"))
+    console.log(doc)
+}
+catch(e){
+    console.error(e)
+}
